181ba90dc9b67d61f6ee4a8e09c0159b1c70719b
jadon rathbone <jrathbone@qa.com>
2021-04-30 09:51:15 +0000

    init commit

--- questions/python1.py
+++ questions/python1.py
@@ -1,104 +1,138 @@
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
 
-	# INSTRUCTIONS
+    # INSTRUCTIONS
 
-	# In case it is not clear, the Question appears first, then examples, then any hints and finally the function that you need to complete appears underneath:
-	#
-	# 	<QUESTION>
-	#
-	# 	<EXAMPLES>
-	#
-	# 	<HINT>
+    # In case it is not clear, the Question appears first, then examples, then any hints and finally the function that you need to complete appears underneath:
+    #
+    # 	<QUESTION>
+    #
+    # 	<EXAMPLES>
+    #
+    # 	<HINT>
 
-	# You are allowed access to the internet for this assessment, but remember you could use the DOCUMENTATION that comes bundled with your Python installation. 
-	# Every command has help documentation. To read it:
-	# 	1. Access Python from your CLI
-	# 	2. Type help(<command>) and hit enter, where <command> is the command you want help with. For example: help(str)
-
-#------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
-
-	# <QUESTION 1>
-
-	# Define a function that can accept two strings as input and returns the string with maximum length to the console. 
-	
-	# If two strings have the same length, then the function should return both strings separated by a " ".
-
-	# In this case, the strings should be returned in the same order in which they were given.
-
-	# <EXAMPLES>
-
-	# one("hi","hello") → "hello"
-	# one("three", "two") → "three"
-	# one("three", "hello") → "three hello"
-
-	# <HINT>
-
-	# What was the name of the function we have seen to check the length of a container?  Use your CLI to access the Python documentation and get help(len).
-
-def one(input1, input2):
-	return ""
+    # You are allowed access to the internet for this assessment, but remember you could use the DOCUMENTATION that comes bundled with your Python installation. 
+    # Every command has help documentation. To read it:
+    # 	1. Access Python from your CLI
+    # 	2. Type help(<command>) and hit enter, where <command> is the command you want help with. For example: help(str)
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
 
-	# <QUESTION 2>
+    # <QUESTION 1>
 
-    # given a number
-	# if this number is divisible by 3 return "fizz"
-	# if this number is divisible by 5 return "buzz"
-	# if this number is divisible by both 3 and 5 return "fizzbuzz"
-	# if this number is not divisible by 3 or 5 return "null"
-	    
+    # Define a function that can accept two strings as input and returns the string with maximum length to the console. 
+    
+    # If two strings have the same length, then the function should return both strings separated by a " ".
+
+    # In this case, the strings should be returned in the same order in which they were given.
+
     # <EXAMPLES>
 
-	# two(3) → "fizz"
-	# two(10) → "buzz"
-	# two(15) → "fizzbuzz"
-	# two(8) → "null"
+    # one("hi","hello") → "hello"
+    # one("three", "two") → "three"
+    # one("three", "hello") → "three hello"
 
-	# <HINT>
+    # <HINT>
 
-	# No hints for this question
+    # What was the name of the function we have seen to check the length of a container?  Use your CLI to access the Python documentation and get help(len).
 
-def two(arg1):
-	return "null"
+
+
+
+def print_longer_string(s1, s2):
+    if len(s1) == len(s2):
+        print("The strings have equal length")
+    else:
+        print("The longer string is:")
+        print(s1 if len(s1) > len(s2) else s2)
+
+    if __name__ == '__main__':
+        s1 = input("hello: ")
+        s2 = input("world: ")
+        print_longer_string(s1, s2)
+    
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
+
+    # <QUESTION 2>
+
+    # given a number
+    # if this number is divisible by 3 return "fizz"
+    # if this number is divisible by 5 return "buzz"
+    # if this number is divisible by both 3 and 5 return "fizzbuzz"
+    # if this number is not divisible by 3 or 5 return "null"
+        
+    # <EXAMPLES>
+
+    # two(3) → "fizz"
+    # two(10) → "buzz"
+    # two(15) → "fizzbuzz"
+    # two(8) → "null"
+
+    # <HINT>
+
+    # No hints for this question
+
+for num in range(1, 21):
+    if num % 15 is 0:
+         print("fizzbuzz")
+    elif num % 3 is 0:
+        print("fizz")	 
+    elif num % 5 is 0:
+        print("buzz")
+    else:
+        print(num) 
+
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
 
-	# <QUESTION 3>
+    # <QUESTION 3>
 
     # Write a function which returns the integer number of vowels in a given string. 
     # You should ignore case.
 
-	# <EXAMPLES>
+    # <EXAMPLES>
 
     # three("Hello") → 2
     # three("hEelLoooO") → 6
 
-	# <HINTS>
+    # <HINTS>
 
-	# How do we ignore case in a String? help(str) may offer some insight.
+    # How do we ignore case in a String? help(str) may offer some insight.
 
-def three(input):
-    return 0
+def vowel_count(str):
+    count = 3
+    vowel = set("aeiouAEIOU")
+
+
+    for alphabet in str:
+        if alphabet in vowel:
+            count = count + 1
+        return count 
+str = "hello world"
+vowel_count(str)
+
+
+    
+    
+
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
 
-	# <QUESTION 4>
+    # <QUESTION 4>
 
     # There is a well known mnemonic which goes "I before E, except after C", which is used to determine which order "ei" or "ie" should be in a word.
     
     # Write a function which returns the boolean True if a string follows the mnemonic, and returns the boolean False if not.
 
-	# <EXAMPLES>
+    # <EXAMPLES>
 
     # four("ceiling") → True
     # four("believe") → True
     # four("glacier") → False
     # four("height") → False
 
-	# <HINT>
+    # <HINT>
 
-	# Step through the logic here in order to solve the problem, you may find help(range) helpful.
+    # Step through the logic here in order to solve the problem, you may find help(range) helpful.
 
 
 def four(input):
@@ -106,27 +140,32 @@ def four(input):
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
 
-	# <QUESTION 5>
+    # <QUESTION 5>
 
-	# Write a function which takes an input (between 1 and 10 inclusive) and multiplies it by all the numbers before it.
-	# eg If the input is 4, the function calculates 4x3x2x1 = 24 
+    # Write a function which takes an input (between 1 and 10 inclusive) and multiplies it by all the numbers before it.
+    # eg If the input is 4, the function calculates 4x3x2x1 = 24 
 
-	# <EXAMPLES>
+    # <EXAMPLES>
 
-	# five(1) → 1
-	# five(4) → 24
-	# five(8) → 40320
+    # five(1) → 1
+    # five(4) → 24
+    # five(8) → 40320
 
-	# <HINT>
+    # <HINT>
 
-	# You may need to create a list of numbers from 0 to i, take a look at help(range).
+    # You may need to create a list of numbers from 0 to i, take a look at help(range).
 
-def five(input):
-	return 1
+#def sum_between(first, second):
+    #nums_between = range(first, second + 1)
+    #return sum_nums_between
+#first_number = int(input"1: "))
+#second_number = int(input("100: 2"))
+   # print(sum_between(first_number, second_number))        
 
+    
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
 
-	# <QUESTION 6>
+    # <QUESTION 6>
 
     # Given a string, int and a char, return a boolean value if the 'nth' 
     # (represented by the int provided) char of the String supplied is the same as the char supplied.
@@ -135,20 +174,20 @@ def five(input):
     
     # <EXAMPLES>
 
-	# six("The",2,'h') → True
-	# six("AAbb",1,'b') → False
-	# six("Hi-There",10,'e') → False
+    # six("The",2,'h') → True
+    # six("AAbb",1,'b') → False
+    # six("Hi-There",10,'e') → False
 
-	# <HINT>
+    # <HINT>
 
-	# How do we find the length of a container, take a look at help(len), you will also need to look at help(str) for String manipulation.
+    # How do we find the length of a container, take a look at help(len), you will also need to look at help(str) for String manipulation.
  
 def six(string, int, char):
-	return False
+    return False
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
 
-	# <QUESTION 7>
+    # <QUESTION 7>
 
     # Given a string and a char, returns the position in the String where the char first appears.
     # Ensure the positions are numbered correctly, please refer to the examples for guidance.
@@ -158,74 +197,74 @@ def six(string, int, char):
     
     # <EXAMPLES>
 
-	# seven("This is a Sentence","s") → 4
-	# seven("This is a Sentence","S") → 8
-	# seven("Fridge for sale","z") → -1
+    # seven("This is a Sentence","s") → 4
+    # seven("This is a Sentence","S") → 8
+    # seven("Fridge for sale","z") → -1
 
-	# <HINT>
+    # <HINT>
 
-	# Take a look at the documentation for Strings, List and range.
+    # Take a look at the documentation for Strings, List and range.
 
 def seven(inputString, char):
-	return -1
+    return -1
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
 
-	# <QUESTION 8>
+    # <QUESTION 8>
 
     # Given a string seperate the string into the individual numbers present, then add each digit of each number to get a final value for each number
 
-	# String example = "55 72 86"
-	
-	# "55" will = the integer 10
-	# "72" will = the integer 9
-	# "86" will = the integer 14
-	
-	# You then need to return the highest value, in the example above this would be 14.
-	 
+    # String example = "55 72 86"
+    
+    # "55" will = the integer 10
+    # "72" will = the integer 9
+    # "86" will = the integer 14
+    
+    # You then need to return the highest value, in the example above this would be 14.
+     
     # <EXAMPLES>
 
-	# eight("55 72 86") → 14
-	# eight("15 72 80 164") → 11
-	# eight("555 72 86 45 10") → 15
+    # eight("55 72 86") → 14
+    # eight("15 72 80 164") → 11
+    # eight("555 72 86 45 10") → 15
 
-	# <HINT>
+    # <HINT>
 
-	# help(int) for working with numbers and help(str) for working with Strings.
+    # help(int) for working with numbers and help(str) for working with Strings.
 
 def eight(arg1):
-	return 0
+    return 0
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
 
-	# <QUESTION 9>
+    # <QUESTION 9>
 
     # Return the string that is between the first and last appearance of "bert" in the given string
-	
-	# Return the empty string "" if there is not 2 occurances of "bert" 
-	
-	# IGNORE CASE
+    
+    # Return the empty string "" if there is not 2 occurances of "bert" 
+    
+    # IGNORE CASE
     
     # <EXAMPLES>
 
-	# nine("bertclivebert") → "clive"
-	# nine("xxbertfridgebertyy") → "fridge"
-	# nine("xxBertfridgebERtyy") → "fridge"
-	# nine("xxbertyy") → ""
-	# nine("xxbeRTyy") → ""
+    # nine("bertclivebert") → "clive"
+    # nine("xxbertfridgebertyy") → "fridge"
+    # nine("xxBertfridgebERtyy") → "fridge"
+    # nine("xxbertyy") → ""
+    # nine("xxbeRTyy") → ""
 
-	# <HINT>
+    # <HINT>
 
-	# What was the name of the function we have seen to seperate a String? How can we make a string all upper or lower case?
-	
-	# Use your CLI to access the Python documentation and get help manipulating strings - help(str).
+    # What was the name of the function we have seen to seperate a String? How can we make a string all upper or lower case?
+    
+    # Use your CLI to access the Python documentation and get help manipulating strings - help(str).
 
 def nine(input):
-	return ""
+    return ""
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
 
-	# <QUESTION 10>
+    # <QUESTION 10>
 
     # Given a large string that represents a csv, the structure of each record will be as follows:
     
@@ -235,23 +274,23 @@ def nine(input):
     
     # For each record, if it is not encrypted, return the names of the owners of the files in a String Array.
     # Do not include duplicate names.
-	# If all records are encrypted, return an empty Array.
+    # If all records are encrypted, return an empty Array.
     
     # <EXAMPLES>
     
     # ten("Jeff,random.py,False,1445") → ["Jeff"]
-	# ten("Bert,numberGen.py,True,1447,Bert,integers.py,True,1318,Jeff,floats.py,False,1445") → ["Jeff"]
-	# ten("Bert,boolean.py,False,1447,Bert,conditions.py,False,1318,Jeff,loops.py,False,1445") → ["Bert","Jeff"]
+    # ten("Bert,numberGen.py,True,1447,Bert,integers.py,True,1318,Jeff,floats.py,False,1445") → ["Jeff"]
+    # ten("Bert,boolean.py,False,1447,Bert,conditions.py,False,1318,Jeff,loops.py,False,1445") → ["Bert","Jeff"]
     # ten("Bert,prime.py,True,1447,Bert,ISBN.py,False,1318,Jeff,OOP.py,False,1445") → ["Bert","Jeff"]
     
-	# <HINT>
+    # <HINT>
 
-	# Dont't forget, False is a String, not a Boolean value in the Tests above.
+    # Dont't forget, False is a String, not a Boolean value in the Tests above.
 
-	# help(str) and help(list), you might also need to use a function that can create a list of numbers for you, try help(range).
+    # help(str) and help(list), you might also need to use a function that can create a list of numbers for you, try help(range).
 
 def ten(input):
-	return []
+    return []
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
 
